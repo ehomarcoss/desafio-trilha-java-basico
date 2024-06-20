e1e520ebc8de0d8e4dedc5661352f824f3b40813
Marcos Rebello <markkellmao@gmail.com>
2024-06-20 18:51:45 -0300

    alteração 1

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner;
+
+        //Exibir as mensagens para o nosso usuario;
+
+        //Obter pela scanner os valores digitados no terminal;
+
+        //Exibir a mensagem conta criada;
     }
 }
